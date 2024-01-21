7fcd29141b375283a8d00866b8eb5edd224de48c
Eyad Kobatte <eyad.kobatte@meltwater.com>
2024-01-21 15:14:08 +0000

    long lived benthos

--- outside-repo/create-configmap.sh
+++ outside-repo/create-configmap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-minikube kubectl -- create configmap benthos-noop-configmap --from-file=./benthos.yaml 
+minikube kubectl -- create configmap benthos-noop-configmap --from-file=./benthos.yaml --dry-run -o yaml | minikube kubectl -- apply -f -
